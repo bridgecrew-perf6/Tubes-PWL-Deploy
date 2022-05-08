c1c878e653f559cc821a5c687a26d3d9ae5dc62f
sayyidalharis <101546647+sayyidalharis@users.noreply.github.com>
2022-05-08 12:46:11 +0700

    Update LikeController.php

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Like;
+use App\Models\User;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
